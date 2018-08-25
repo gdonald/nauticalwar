4699727894ffc28091b4290d5570751ce4d44dd1
Greg Donald <gdonald@gmail.com>
2018-08-24 21:40:21 -0500

    fix

--- app/models/enemy.rb
+++ app/models/enemy.rb
@@ -1,7 +1,7 @@
 class Enemy < ApplicationRecord
 
-  belongs_to :user_1, class_name 'User', foreign_key: 'user_1_id'
-  belongs_to :user_2, class_name 'User', foreign_key: 'user_2_id'
+  belongs_to :user_1, class_name: 'User', foreign_key: 'user_1_id'
+  belongs_to :user_2, class_name: 'User', foreign_key: 'user_2_id'
   
   validates :user_1, presence: true
   validates :user_2, presence: true
