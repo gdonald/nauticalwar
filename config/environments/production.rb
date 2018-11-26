f3e338c974a65b622bd40b8f82f36ca3f64f5170
Greg Donald <gdonald@gmail.com>
2018-11-25 18:35:27 -0600

    mail url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and
   # use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
@@ -81,10 +81,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to
   # raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-
-  config.action_mailer.default_url_options = { host: 'nauticalwar.com',
-                                               port: 443 }
-
+  config.action_mailer.default_url_options = { host: 'nauticalwar.com' }
   config.action_mailer.delivery_method = :sendmail
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
