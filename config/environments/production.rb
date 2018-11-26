98f623c7f1a909eebc7609425109e46e90df5c13
Greg Donald <gdonald@gmail.com>
2018-11-25 18:29:50 -0600

    sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'nauticalwar.com',
                                                port: 443 }
 
+  config.action_mailer.delivery_method = :sendmail
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
