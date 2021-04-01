0921ab60d416d8818790fd7645983a5bf3e9d322
Greg Donald <gdonald@gmail.com>
2021-03-31 20:16:32 -0500

    security updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  after_action :custom_headers
+
   def current_admin
     @current_admin ||= Player.find_by(id: session[:admin_id])
   end
@@ -21,5 +23,9 @@ class ApplicationController < ActionController::Base
 
   def access_denied(_params)
     redirect_to new_admin_session_path
+  end
+
+  def custom_headers
+    response.headers['Pragma'] = 'no-cache'
   end
 end
